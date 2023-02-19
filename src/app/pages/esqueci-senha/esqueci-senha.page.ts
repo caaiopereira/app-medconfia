c7c3aaabc3164bd27d1606899ac1af379edc6f29
caaiopereira <caaiopereira9@gmail.com>
2023-02-18 22:30:46 -0300

    Update sweetalert, resetpassword

--- src/app/pages/esqueci-senha/esqueci-senha.page.ts
+++ src/app/pages/esqueci-senha/esqueci-senha.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 import { AuthService } from 'src/servico/auth.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-esqueci-senha',
@@ -11,15 +14,42 @@ export class EsqueciSenhaPage implements OnInit {
   
   constructor(
     private authentication: AuthService,
+    private alertController: AlertController,
+    private router: Router,
   ) { }
 
   ngOnInit() {
   }
 
-  recoverPassword(form: any){
-    this.authentication.resetPassword(form.value.email);
+  async recoverPassword(form: any) {
+    try {
+      await this.authentication.resetPassword(form.value.email);
+      await this.alertController.create({
+      });
+      Swal.fire({
+        title: 'Pronto!',
+        text: 'Um email foi enviado para redefinir a senha.',
+        icon: 'success',
+        heightAuto: false
+      });
+      this.router.navigate(['tour']);
+    } catch (error) {
+      let message = '';
+      if (error.code === 'auth/missing-email') {
+        message = 'Preencha o campo corretamente  ';
+      } else {
+        message = 'Email inválido ou não cadastrado.';
+      }
+      Swal.fire({
+        title: 'Oops!',
+        text: message,
+        icon: 'error',
+        heightAuto: false
+      });
+    }
   }
-  
+
+
 }
 
       
